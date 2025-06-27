90b9b1f22194719fe34881ea5c9bf64a8a942e8c
kingRdy <rudyanto361@gmail.com>
2025-06-27 16:28:35 +0700

    Create PL mastertv.tech0909-aspalLIVE1-Petir-petir.php

---
+++ PL mastertv.tech0909-aspalLIVE1-Petir-petir.php
@@ -0,0 +1,65 @@
+#EXTM3U
+#EXTINF:-1 tvg-logo="https://kilatlive.live/public/logo.png" group-title="JADWAL LIVE KILAT",Tottenham vs Everton - 22:00 WIB 24-05-2026
+https://rr5---sn-nx57ynsk.googlecdncloud.com/public/kilat21_6.mp4
+#EXTINF:-1 tvg-logo="https://kilatlive.live/public/logo.png" group-title="JADWAL LIVE KILAT",AS Roma vs Bologna - 20:00 WIB 24-08-2025
+https://rr5---sn-nx57ynsk.googlecdncloud.com/public/kilat21_6.mp4
+#EXTINF:-1 tvg-logo="https://kilatlive.live/public/logo.png" group-title="JADWAL LIVE KILAT",Moldova vs Italy - 02:45 WIB 14-11-2025
+https://rr5---sn-nx57ynsk.googlecdncloud.com/public/kilat21_6.mp4
+#EXTINF:-1 tvg-logo="https://kilatlive.live/public/logo.png" group-title="JADWAL LIVE KILAT",England vs Andorra - 23:00 WIB 06-09-2025
+https://rr5---sn-nx57ynsk.googlecdncloud.com/public/kilat21_6.mp4
+#EXTINF:-1 tvg-logo="https://kilatlive.live/public/logo.png" group-title="JADWAL LIVE KILAT",NAC Breda vs Excelsior - 23:00 WIB 07-02-2026
+https://rr5---sn-nx57ynsk.googlecdncloud.com/public/kilat21_6.mp4
+#EXTINF:-1 tvg-logo="https://kilatlive.live/public/logo.png" group-title="JADWAL LIVE KILAT",AC Milan vs Juventus - 20:00 WIB 26-04-2026
+https://rr5---sn-nx57ynsk.googlecdncloud.com/public/kilat21_6.mp4
+#EXTINF:-1 tvg-logo="https://kilatlive.live/public/logo.png" group-title="JADWAL LIVE KILAT",West Ham vs Leeds - 22:00 WIB 24-05-2026
+https://rr5---sn-nx57ynsk.googlecdncloud.com/public/kilat21_6.mp4
+#EXTINF:-1 tvg-logo="https://kilatlive.live/public/logo.png" group-title="JADWAL LIVE KILAT",AC Milan vs Bologna - 20:00 WIB 14-09-2025
+https://rr5---sn-nx57ynsk.googlecdncloud.com/public/kilat21_6.mp4
+#EXTINF:-1 tvg-logo="https://kilatlive.live/public/logo.png" group-title="JADWAL LIVE KILAT",Greece vs Scotland - 02:45 WIB 16-11-2025
+https://rr5---sn-nx57ynsk.googlecdncloud.com/public/kilat21_6.mp4
+#EXTINF:-1 tvg-logo="https://kilatlive.live/public/logo.png" group-title="JADWAL LIVE KILAT",AC Milan vs Lazio - 21:00 WIB 30-11-2025
+https://rr5---sn-nx57ynsk.googlecdncloud.com/public/kilat21_6.mp4
+#EXTINF:-1 tvg-logo="https://kilatlive.live/public/logo.png" group-title="JADWAL LIVE KILAT",Heracles vs Feyenoord - 21:30 WIB 18-10-2025
+https://rr5---sn-nx57ynsk.googlecdncloud.com/public/kilat21_6.mp4
+#EXTINF:-1 tvg-logo="https://kilatlive.live/public/logo.png" group-title="JADWAL LIVE KILAT",Tottenham vs Brighton - 21:00 WIB 18-04-2026
+https://rr5---sn-nx57ynsk.googlecdncloud.com/public/kilat21_6.mp4
+#EXTINF:-1 tvg-logo="https://kilatlive.live/public/logo.png" group-title="JADWAL LIVE KILAT",Fiorentina vs Pisa - 21:00 WIB 22-02-2026
+https://rr5---sn-nx57ynsk.googlecdncloud.com/public/kilat21_6.mp4
+#EXTINF:-1 tvg-logo="https://kilatlive.live/public/logo.png" group-title="JADWAL LIVE KILAT",NAC Breda vs GO Ahead Eagles - 00:45 WIB 02-11-2025
+https://rr5---sn-nx57ynsk.googlecdncloud.com/public/kilat21_6.mp4
+#EXTINF:-1 tvg-logo="https://kilatlive.live/public/logo.png" group-title="JADWAL LIVE KILAT",AZ Alkmaar vs Utrecht - 22:45 WIB 26-10-2025
+https://rr5---sn-nx57ynsk.googlecdncloud.com/public/kilat21_6.mp4
+#EXTINF:-1 tvg-logo="https://kilatlive.live/public/logo.png" group-title="JADWAL LIVE KILAT",Udinese vs AC Milan - 20:00 WIB 21-09-2025
+https://rr5---sn-nx57ynsk.googlecdncloud.com/public/kilat21_6.mp4
+#EXTINF:-1 tvg-logo="https://kilatlive.live/public/logo.png" group-title="JADWAL LIVE KILAT",Aston Villa vs Fulham - 21:00 WIB 27-09-2025
+https://rr5---sn-nx57ynsk.googlecdncloud.com/public/kilat21_6.mp4
+#EXTINF:-1 tvg-logo="https://kilatlive.live/public/logo.png" group-title="JADWAL LIVE KILAT",PEC Zwolle vs GO Ahead Eagles - 17:15 WIB 21-09-2025
+https://rr5---sn-nx57ynsk.googlecdncloud.com/public/kilat21_6.mp4
+#EXTINF:-1 tvg-logo="https://kilatlive.live/public/logo.png" group-title="JADWAL LIVE KILAT",Brentford vs Sunderland - 03:00 WIB 08-01-2026
+https://rr5---sn-nx57ynsk.googlecdncloud.com/public/kilat21_6.mp4
+#EXTINF:-1 tvg-logo="https://kilatlive.live/public/logo.png" group-title="JADWAL LIVE KILAT",Brighton vs Burnley - 22:00 WIB 03-01-2026
+https://rr5---sn-nx57ynsk.googlecdncloud.com/public/kilat21_6.mp4
+#EXTINF:-1 tvg-logo="https://kilatlive.live/public/logo.png" group-title="JADWAL LIVE KILAT",Torino vs AS Roma - 21:00 WIB 18-01-2026
+https://rr5---sn-nx57ynsk.googlecdncloud.com/public/kilat21_6.mp4
+#EXTINF:-1 tvg-logo="https://kilatlive.live/public/logo.png" group-title="JADWAL LIVE KILAT",Ajax vs Telstar - 19:30 WIB 10-08-2025
+https://rr5---sn-nx57ynsk.googlecdncloud.com/public/kilat21_6.mp4
+#EXTINF:-1 tvg-logo="https://kilatlive.live/public/logo.png" group-title="JADWAL LIVE KILAT",Cliftonville FC vs St Joseph S Fc - 01:45 WIB 18-07-2025
+https://rr5---sn-nx57ynsk.googlecdncloud.com/public/kilat21_6.mp4
+#EXTINF:-1 tvg-logo="https://kilatlive.live/public/logo.png" group-title="JADWAL LIVE KILAT",Arsenal vs Manchester City - 21:00 WIB 20-09-2025
+https://rr5---sn-nx57ynsk.googlecdncloud.com/public/kilat21_6.mp4
+#EXTINF:-1 tvg-logo="https://kilatlive.live/public/logo.png" group-title="JADWAL LIVE KILAT",Heracles vs FC Volendam - 22:00 WIB 23-04-2026
+https://rr5---sn-nx57ynsk.googlecdncloud.com/public/kilat21_6.mp4
+#EXTINF:-1 tvg-logo="https://kilatlive.live/public/logo.png" group-title="JADWAL LIVE KILAT",Udinese vs Parma - 20:00 WIB 19-04-2026
+https://rr5---sn-nx57ynsk.googlecdncloud.com/public/kilat21_6.mp4
+#EXTINF:-1 tvg-logo="https://kilatlive.live/public/logo.png" group-title="JADWAL LIVE KILAT",GO Ahead Eagles vs Ajax - 17:15 WIB 17-08-2025
+https://rr5---sn-nx57ynsk.googlecdncloud.com/public/kilat21_6.mp4
+#EXTINF:-1 tvg-logo="https://kilatlive.live/public/logo.png" group-title="JADWAL LIVE KILAT",Liverpool vs Tottenham - 22:00 WIB 14-03-2026
+https://rr5---sn-nx57ynsk.googlecdncloud.com/public/kilat21_6.mp4
+#EXTINF:-1 tvg-logo="https://kilatlive.live/public/logo.png" group-title="JADWAL LIVE KILAT",St Joseph S Fc vs Cliftonville FC - 23:00 WIB 08-07-2025
+https://rr5---sn-nx57ynsk.googlecdncloud.com/public/kilat21_6.mp4
+#EXTINF:-1 tvg-logo="https://kilatlive.live/public/logo.png" group-title="JADWAL LIVE KILAT",Udinese vs Bologna - 21:00 WIB 23-11-2025
+https://rr5---sn-nx57ynsk.googlecdncloud.com/public/kilat21_6.mp4
+#EXTINF:-1 tvg-logo="https://kilatlive.live/public/logo.png" group-title="JADWAL LIVE KILAT",PSV Eindhoven vs GO Ahead Eagles - 23:45 WIB 18-10-2025
+https://rr5---sn-nx57ynsk.googlecdncloud.com/public/kilat21_6.mp4
+#EXTINF:-1 tvg-logo="https://kilatlive.live/public/logo.png" group-title="JADWAL LIVE KILAT",Sassuolo vs Torino - 21:00 WIB 21-12-2025
+https://rr5---sn-nx57ynsk.googlecdncloud.com/public/kilat21_6.mp4
