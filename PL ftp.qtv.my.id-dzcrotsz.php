414ebfbac5d5890c187627086011805d6fa0d914
kingRdy <rudyanto361@gmail.com>
2025-06-27 16:04:30 +0700

    Create PL ftp.qtv.my.id-dzcrotsz.php

---
+++ PL ftp.qtv.my.id-dzcrotsz.php
@@ -0,0 +1,48 @@
+#KODIPROP:inputstream.adaptive.license_type=com.widevine.alpha
+#KODIPROP:inputstream.adaptive.license_key=http://ftp.qtv.my.id/drm.kt?id=2vzlkc4qb5l6osfb0yen08har&type=drm&token=7cd659d8b63b417885b99da153accea5
+#EXTVLCOPT:http-user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36
+#EXTINF:-1 group-title="📁 CWC UpComing" tvg-logo="http://ftp.qtv.my.id/K8EmvP6vh9fDDInKKaEw7Y@jvNBx0M@L/lInErMdlZCQ3DQWarBFw1YI2rgsLy1@L.png" group-logo="https://crystalpng.com/wp-content/uploads/2025/03/fifa_club_world_cup_logo.png",Palmeiras vs. Botafogo [HD] (28/06 23:00 WIB)
+http://ftp.qtv.my.id/manifest.mpd?id=2vzlkc4qb5l6osfb0yen08har&type=dash&token=7cd659d8b63b417885b99da153accea5
+
+#KODIPROP:inputstream.adaptive.license_type=com.widevine.alpha
+#KODIPROP:inputstream.adaptive.license_key=http://ftp.qtv.my.id/drm.kt?id=civjin8j2bouuiq5nt5add2g9&type=drm&token=7cd659d8b63b417885b99da153accea5
+#EXTVLCOPT:http-user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36
+#EXTINF:-1 group-title="📁 CWC UpComing" tvg-logo="http://ftp.qtv.my.id/rNAVocBymYe2wBRMUigwZdG5ysMhghiR/tWlx0u~vc6Vh~Dv@Sa1xFGNTpuyecr@c/wzaymSavIRsIFCaa4ff7rL1yrNkFmLWS.png" group-logo="https://crystalpng.com/wp-content/uploads/2025/03/fifa_club_world_cup_logo.png",Palmeiras vs. Botafogo [FHD] (28/06 23:00 WIB)
+http://ftp.qtv.my.id/manifest.mpd?id=civjin8j2bouuiq5nt5add2g9&type=dash&token=7cd659d8b63b417885b99da153accea5
+
+#KODIPROP:inputstream.adaptive.license_type=com.widevine.alpha
+#KODIPROP:inputstream.adaptive.license_key=http://ftp.qtv.my.id/drm.kt?id=c2v6urpv8vn1m99s8p8l6smyh&type=drm&token=7cd659d8b63b417885b99da153accea5
+#EXTVLCOPT:http-user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36
+#EXTINF:-1 group-title="📁 CWC UpComing" tvg-logo="http://ftp.qtv.my.id/QCw7PsjeuRpeoS2bB7lFD03cEn8s2kVB/cEngz1VOVopVfa0VZinDncoG~oc6YtmX.png" group-logo="https://crystalpng.com/wp-content/uploads/2025/03/fifa_club_world_cup_logo.png",SL Benfica vs. Chelsea FC [HD] (29/06 03:00 WIB)
+http://ftp.qtv.my.id/manifest.mpd?id=c2v6urpv8vn1m99s8p8l6smyh&type=dash&token=7cd659d8b63b417885b99da153accea5
+
+#KODIPROP:inputstream.adaptive.license_type=com.widevine.alpha
+#KODIPROP:inputstream.adaptive.license_key=http://ftp.qtv.my.id/drm.kt?id=cx4a9a1q5f66jfn8n3yf4tvre&type=drm&token=7cd659d8b63b417885b99da153accea5
+#EXTVLCOPT:http-user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36
+#EXTINF:-1 group-title="📁 CWC UpComing" tvg-logo="http://ftp.qtv.my.id/EExzWGCE4Oga1pqRDc~jDEp0Q6XQxXsD/GvHLccgl~jHTcadPN6xOskPGw21U9T2x/nSGoo2dAay2rzFwUfe888Yl8a4gx4FtP.png" group-logo="https://crystalpng.com/wp-content/uploads/2025/03/fifa_club_world_cup_logo.png",SL Benfica vs. Chelsea FC [FHD] (29/06 03:00 WIB)
+http://ftp.qtv.my.id/manifest.mpd?id=cx4a9a1q5f66jfn8n3yf4tvre&type=dash&token=7cd659d8b63b417885b99da153accea5
+
+#KODIPROP:inputstream.adaptive.license_type=com.widevine.alpha
+#KODIPROP:inputstream.adaptive.license_key=http://ftp.qtv.my.id/drm.kt?id=bqr2aoyffez3xkqrdkoz1jkc5&type=drm&token=7cd659d8b63b417885b99da153accea5
+#EXTVLCOPT:http-user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36
+#EXTINF:-1 group-title="📁 CWC UpComing" tvg-logo="http://ftp.qtv.my.id/psjrRP9pnecGKX~VOKhN0UEYdrT~RNwd/zqjhPvqUSKWWYfIqc2LbtIzxyOLGdU8f.png" group-logo="https://crystalpng.com/wp-content/uploads/2025/03/fifa_club_world_cup_logo.png",Paris Saint-Germain vs. Inter Miami CF [HD] (29/06 23:00 WIB)
+http://ftp.qtv.my.id/manifest.mpd?id=bqr2aoyffez3xkqrdkoz1jkc5&type=dash&token=7cd659d8b63b417885b99da153accea5
+
+#KODIPROP:inputstream.adaptive.license_type=com.widevine.alpha
+#KODIPROP:inputstream.adaptive.license_key=http://ftp.qtv.my.id/drm.kt?id=4ylz50tslhpf17icjvtkg2s1y&type=drm&token=7cd659d8b63b417885b99da153accea5
+#EXTVLCOPT:http-user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36
+#EXTINF:-1 group-title="📁 CWC UpComing" tvg-logo="http://ftp.qtv.my.id/UnpA5XpJpujMk2Ihgkd1iTneyR8Ovip5/yEoxt5bbmRXU52vdnaAWZpzTweFKpedE/Qz~eGBKAOELIcd~wMtTG7SC6MSz12GEv.png" group-logo="https://crystalpng.com/wp-content/uploads/2025/03/fifa_club_world_cup_logo.png",Paris Saint-Germain vs. Inter Miami CF [FHD] (29/06 23:00 WIB)
+http://ftp.qtv.my.id/manifest.mpd?id=4ylz50tslhpf17icjvtkg2s1y&type=dash&token=7cd659d8b63b417885b99da153accea5
+
+#KODIPROP:inputstream.adaptive.license_type=com.widevine.alpha
+#KODIPROP:inputstream.adaptive.license_key=http://ftp.qtv.my.id/drm.kt?id=3qy5a9rrqgikuaxxdtx2s317h&type=drm&token=7cd659d8b63b417885b99da153accea5
+#EXTVLCOPT:http-user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36
+#EXTINF:-1 group-title="📁 CWC UpComing" tvg-logo="http://ftp.qtv.my.id/1NuovxV5krZN2BDEjjymma~ow7SRChtl/CtGN3yHYn2secsPRogxwg1lRhwO3GoFs.png" group-logo="https://crystalpng.com/wp-content/uploads/2025/03/fifa_club_world_cup_logo.png",Flamengo vs. Bayern München [HD] (30/06 03:00 WIB)
+http://ftp.qtv.my.id/manifest.mpd?id=3qy5a9rrqgikuaxxdtx2s317h&type=dash&token=7cd659d8b63b417885b99da153accea5
+
+#KODIPROP:inputstream.adaptive.license_type=com.widevine.alpha
+#KODIPROP:inputstream.adaptive.license_key=http://ftp.qtv.my.id/drm.kt?id=vz77nx4jlgy49jppe952a44y&type=drm&token=7cd659d8b63b417885b99da153accea5
+#EXTVLCOPT:http-user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36
+#EXTINF:-1 group-title="📁 CWC UpComing" tvg-logo="http://ftp.qtv.my.id/Pz9h6~6YQIvrJyGB2CLZLfBtkhKtHBar/W5qd8L1BhXGZD82TgDCpEXm@Soh5xDod/mMqTtBg9kW@4MXLv2SOWYQuUz04qeVwS.png" group-logo="https://crystalpng.com/wp-content/uploads/2025/03/fifa_club_world_cup_logo.png",Flamengo vs. Bayern München [FHD] (30/06 03:00 WIB)
+http://ftp.qtv.my.id/manifest.mpd?id=vz77nx4jlgy49jppe952a44y&type=dash&token=7cd659d8b63b417885b99da153accea5
+
